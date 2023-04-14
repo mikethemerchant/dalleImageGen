5a0720e164a60fbaac206b82bcfc298372156665
Michael Bender <michael.bender@spartronics.com>
2023-04-14 11:18:07 -0400

    dall e creates images

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-//https://github.com/mikethemerchant/dalleImageGen.git
 
-//..
+const { Configuration, OpenAIApi } = require("openai");
 
+const config = new Configuration({
+    apiKey: "change this to your api key"
+});
 
-console.log("hello world");
+const openai = new OpenAIApi(config);
+
+const prompt = "mountain view trees and stream in the background";
+const numberOfImages = 1;
+const imageSize = "1024x1024";
+
+openai.createImage({
+    prompt: prompt,
+    n: numberOfImages,
+    size: imageSize
+}).then((data) => {
+    console.log(data.data.data);
+}).catch((error) => {
+    console.log(error);
+});
